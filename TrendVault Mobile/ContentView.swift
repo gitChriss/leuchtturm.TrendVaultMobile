46a973db6994ff6b909efff854e2b554ab1677c1
Christian Ruppelt <post@christianruppelt.de>
2025-12-14 23:06:41 +0100

    Chunk 1 – Project setup

---
+++ TrendVault Mobile/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TrendVault Mobile
+//
+//  Created by Christian Ruppelt on 14.12.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TrendVault Mobile/TrendVault_MobileApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrendVault_MobileApp.swift
+//  TrendVault Mobile
+//
+//  Created by Christian Ruppelt on 14.12.25.
+//
+
+import SwiftUI
+
+@main
+struct TrendVault_MobileApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
